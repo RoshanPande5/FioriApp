3f83c65b8007e2fa80b794946fdf1322f4efb402
RoshanPande5 <roshanpande2233@gmail.com>
2024-12-23 05:36:27 +0000

    added Button

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,14 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller,MessageToast) => {
     "use strict";
 
     return Controller.extend("fioriapp.controller.View1", {
         onInit() {
+        },
+        onPress : function () {
+            MessageToast.show("Button Clicked");
         }
     });
 });
